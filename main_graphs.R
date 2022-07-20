615d351719b23cbc40c209ce89cfbf13fb39bb0e
Eduardo Koerich Nery <eduardo.k.nery@gmail.com>
2022-07-20 15:08:57 -0300

    Update main_graphs.R

--- main_graphs.R
+++ main_graphs.R
@@ -12,6 +12,9 @@ library(RColorBrewer)
 library(reshape2)
 
 ### loading geographic and niche sister data
+sister_no_metrics = read.table("2_sister_hypervolume/sister_no_metrics.csv", sep=',', h=T)
+
+### loading geographic and niche sister data
 sister_ro_metrics = read.table("3_sister_geography/sister_ro_metrics.csv", sep=',', h=T)
 
 ### loading occurrence count per domain
@@ -30,7 +33,7 @@ names(geo_states) = spp_count_domain$species
 mycols = c( "#1E88E5", "#FFC107", "#D81B60")
 
 ### sister ro metrics
-tiff("7_graphs/ro_intercept_geographic_distribution.tiff", units="in", width=3.5, height=3, res=600)
+tiff("7_graphs/RO_intercept_geographic_distribution.tiff", units="in", width=3.5, height=3, res=600)
 ggplot(data= sister_ro_metrics, aes(x=state, y=intercept_ro, fill= state)) +
   geom_point(aes(color=state),position = position_jitter(width = 0.07), size = 1.5, alpha = 0.25) +
   geom_boxplot(width = 0.2, outlier.shape = NA, alpha = 0.50)+
@@ -38,6 +41,19 @@ ggplot(data= sister_ro_metrics, aes(x=state, y=intercept_ro, fill= state)) +
   scale_fill_manual(values=mycols)+
   scale_colour_manual(values=mycols)+
   xlab("geographic distribution")+ ylab("RO intercept")+
+  scale_x_discrete(labels=c("AF" = "AF-endemic", "AFother" = "AF and other\ndomains", "other" = "outside AF"))+
+  theme(panel.background=element_rect(fill="white"), panel.grid=element_line(colour=NULL),panel.border=element_rect(fill=NA,colour="black"),axis.title=element_text(size=14,face="bold"),axis.text.x=element_text(size=8),legend.position = "none")
+dev.off()
+
+### sister no metrics
+tiff("7_graphs/NO_intercept_geographic_distribution.tiff", units="in", width=3.5, height=3, res=600)
+ggplot(data= sister_no_metrics, aes(x=state, y=intercept_no, fill= state)) +
+  geom_point(aes(color=state),position = position_jitter(width = 0.07), size = 1.5, alpha = 0.25) +
+  geom_boxplot(width = 0.2, outlier.shape = NA, alpha = 0.50)+
+  geom_flat_violin(position = position_nudge(x = 0.12, y = 0), alpha = 0.50) +
+  scale_fill_manual(values=mycols)+
+  scale_colour_manual(values=mycols)+
+  xlab("geographic distribution")+ ylab("NO intercept")+
   scale_x_discrete(labels=c("AF" = "AF-endemic", "AFother" = "AF and other\ndomains", "other" = "outside AF"))+
   theme(panel.background=element_rect(fill="white"), panel.grid=element_line(colour=NULL),panel.border=element_rect(fill=NA,colour="black"),axis.title=element_text(size=14,face="bold"),axis.text.x=element_text(size=8),legend.position = "none")
 dev.off()
